b89a006eee57dec4b34c4fe2a5b2e428bf1e6cb5
Eduardo Marín Nicolalde <126416022+edu-marin@users.noreply.github.com>
2024-05-08 20:21:45 +0000

    acero_to_sqlite

--- data_migration/process.py
+++ data_migration/process.py
@@ -16,10 +16,29 @@ dataframes = []
 for sheet_name in sheet_names:
     print(f"Sheet Name: {sheet_name}")
     df = pd.read_excel(my_path, sheet_name=sheet_name)
+    df["source"] = sheet_name
     dataframes.append(df)
     print(f"Shape: {df.shape}")
     print(f"Column Names: {df.columns}\n")
 
 all = pd.concat(dataframes)
-all.info()
+all[all.pvp.isnull()][["codigo","source"]] # pulseras varios no tiene costo ni pvp
 
+
+# insert records in all dataframe into a sqlite db named acero.db  
+import sqlite3        
+conn = sqlite3.connect("data_migration/data/acero.db")
+all.to_sql("joyas", conn, if_exists="replace")
+conn.close()
+
+# read the data from the sqlite db
+conn = sqlite3.connect("data_migration/data/acero.db")
+query = """
+    SELECT codigo, source, costo, pvp
+    FROM joyas
+    WHERE costo IS NULL OR pvp IS NULL
+"""
+df = pd.read_sql(query, conn)
+conn.close()
+
+print(df)
